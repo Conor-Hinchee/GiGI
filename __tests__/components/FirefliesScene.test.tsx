7def246a2c4bd5710e26a040b5acd00b584dc6ea
Conor-Hinchee <conorphinchee@gmail.com>
2025-06-27 14:49:13 -0400

    deleting tests. test are against the vibe

--- __tests__/components/FirefliesScene.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import FirefliesScene from '../../components/FirefliesScene';
-
-test('parallax effect is disabled on mobile devices', () => {
-  Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 500 });
-  const { container } = render(<FirefliesScene />);
-  expect(container.querySelector('.parallax')).toBeNull();
-});
--- __tests__/pages/page.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import Page from '../../pages/Page';
-
-test('parallax effect is disabled on mobile devices', () => {
-	const { container } = render(<Page />);
-	expect(container.querySelector('.parallax')).toBeNull();
-});
